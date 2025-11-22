63bbca0b7b74b07fe6d67eea194773e20dfb79db
Craig <craig.hickman@ukaea.uk>
2025-11-22 00:11:01 +0000

    fix confirm on exit

--- lib/widgets/confirm_on_exit.dart
+++ lib/widgets/confirm_on_exit.dart
@@ -1,10 +1,12 @@
 // confirm_on_exit.dart
+import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ConfirmOnExit extends StatelessWidget {
   final Widget child;
 
-  const ConfirmOnExit({required this.child, super.key});
+  const ConfirmOnExit({super.key, required this.child});
 
   Future<bool> _showConfirmationDialog(BuildContext context) async {
     final result = await showDialog<bool>(
@@ -14,11 +16,11 @@ class ConfirmOnExit extends StatelessWidget {
         content: const Text('Are you sure you want to close?'),
         actions: [
           TextButton(
-            onPressed: () => Navigator.pop(context, false),
+            onPressed: () => Navigator.of(context).pop(false),
             child: const Text('No'),
           ),
           TextButton(
-            onPressed: () => Navigator.pop(context, true),
+            onPressed: () => Navigator.of(context).pop(true),
             child: const Text('Yes'),
           ),
         ],
@@ -27,16 +29,27 @@ class ConfirmOnExit extends StatelessWidget {
     return result ?? false;
   }
 
+  void _exitApp(BuildContext context) {
+    if (Platform.isAndroid) {
+      // Properly closes the activity on Android
+      SystemNavigator.pop();
+    } else {
+      // On iOS, apps are not supposed to exit themselves;
+      // just pop the route if possible.
+      Navigator.of(context).maybePop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return PopScope(
+    return PopScope<Object?>(
       canPop: false,
       onPopInvokedWithResult: (didPop, result) async {
         if (didPop) return;
 
         final shouldExit = await _showConfirmationDialog(context);
         if (shouldExit && context.mounted) {
-          Navigator.pop(context);
+          _exitApp(context);
         }
       },
       child: child,
